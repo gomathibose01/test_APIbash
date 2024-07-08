8c9f1ca27dcb44758dbecd455bf6e4059683a38c
gomathibose01 <167166656+gomathibose01@users.noreply.github.com>
2024-07-08 17:44:55 +0530

    Update ADDPGM.cob

--- cobol/ADDPGM.cob
+++ cobol/ADDPGM.cob
@@ -13,7 +13,7 @@
            ACCEPT WS-Z.
            ACCEPT WS-S.
            COMPUTE WS-TOT = WS-X + WS-Y.                                00060004
-           DISPLAY 'SUM OF ' WS-X ' AND 'WS-Y ' IS:' WS-TOT.            00070001
+           DISPLAY 'SUM OF ' WS-X  ' AND 'WS-Y ' IS:' WS-TOT.            00070001
            IF WS-TOT NOT = WS-Z THEN
               MOVE +12  TO RETURN-CODE
            ELSE
