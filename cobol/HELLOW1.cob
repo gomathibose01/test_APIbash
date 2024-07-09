8604c33c1aac9f33b01b0362a6427ac86931a663
gomathibose01 <167166656+gomathibose01@users.noreply.github.com>
2024-07-09 18:02:36 +0530

    Delete cobol directory

--- cobol/HELLOW1.cob
+++
@@ -1,8 +0,0 @@
-       IDENTIFICATION DIVISION.                              
-       PROGRAM-ID. PGM01.                                    
-       ENVIRONMENT DIVISION.                                 
-       DATA DIVISION.                                        
-       PROCEDURE DIVISION.                                   
-           DISPLAY 'HELLO WORLD'.                            
-           DISPLAY 'THIS IS GOMATHIs POPUP TEST PROGRAM'.    
-           STOP RUN.                                         
