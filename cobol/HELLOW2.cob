6022dfa7b4e7fb2f3a3758ed7b036d0740057550
gomathibose01 <167166656+gomathibose01@users.noreply.github.com>
2024-07-09 18:36:58 +0530

    Add Hellow2 new pgm

---
+++ cobol/HELLOW2.cob
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.                              
+       PROGRAM-ID. HELLOW2.                                    
+       ENVIRONMENT DIVISION.                                 
+       DATA DIVISION.                                        
+       PROCEDURE DIVISION.                                   
+           DISPLAY 'HELLO WORLD'.                            
+           DISPLAY 'THIS IS GOMATHIs POPUP TEST PROGRAM'.    
+           STOP RUN.                                         
