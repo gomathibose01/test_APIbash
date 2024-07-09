11818f6f23185f43f2608b9d951c03188eb9f34f
gomathibose01 <167166656+gomathibose01@users.noreply.github.com>
2024-07-09 16:52:56 +0530

    Delete cobol directory

--- cobol/HELLOGB.cob
+++
@@ -1,8 +0,0 @@
-       IDENTIFICATION DIVISION.                              
-       PROGRAM-ID. HELLOGB.                                    
-       ENVIRONMENT DIVISION.                                 
-       DATA DIVISION.                                        
-       PROCEDURE DIVISION.                                   
-           DISPLAY 'HELLO 3 WORLD'.                            
-           DISPLAY 'THIS IS GOMATHIs POPUP TEST PROGRAM'.    
-           STOP RUN.                                         
--- cobol/HELLON.cob
+++
@@ -1,8 +0,0 @@
-       IDENTIFICATION DIVISION.                              
-       PROGRAM-ID. HELLON.                                    
-       ENVIRONMENT DIVISION.                                 
-       DATA DIVISION.                                        
-       PROCEDURE DIVISION.                                   
-           DISPLAY 'HELLO NEW WORLD'.                            
-           DISPLAY 'THIS IS GOMATHIs POPUP TEST PROGRAM'.    
-           STOP RUN.                                         
